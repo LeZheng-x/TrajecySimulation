92dc29b7593b150d23c9c16c1030f3b89f0ae993
Le.Zheng <2360838113@qq.com>
2023-11-24 21:42:32 +0800

    add gif and update .md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TrajecySimulation
 Use Matlab to simulate the two dimensional motion trajectories of nine planets .
+###how to use
+Just run the file in Matlab app,then you will see an image is being drawn.
+###description
+Since the nine planets have different parameters such as mass and eccentricity, their motion periods can be calculated separately according to Kepler's three laws and simulated and displayed through code.
+###Show results 
+![image](./plants.gif)
+####Notes:
+Please zoom in while the program is running to see all the planetary trajectories 
--- dynamicDisplay.m
+++ dynamicDisplay.m
@@ -3,6 +3,9 @@ clc;
 clear;
 planets = {'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'};
 
+ifSaveGif =1; %是否保存gif
+sleep = 0.05; %gif卡顿时间 单位：S
+deltaTheta = 2.5; %角度变化倍数
 
 a = [0.387 0.723 1 1.524 5.203 9.582 19.189 30.069 39.482]; % 半长轴
 e = [0.2056 0.0068 0.0167 0.0934 0.0483 0.056 0.045 0.0113 0.2488]; % 偏心率
@@ -24,7 +27,6 @@ text(SunX,SunY,'Sun');
 % % 生成椭圆上的点的坐标
 t = linspace(0, 2*pi, frame); % 角度范围
 
-sleep = 0.01;
 
 
 
@@ -40,15 +42,30 @@ end
 
 %绘制动态图
 theta = zeros(1,length(a));
+pic_num=1;
 
-for j  = 1:300
+
+
+for j  = 1:200
     for i = 1:length(a)
-        theta(i) = 2*pi*j/T(i);
+        theta(i) = 2*pi*j/T(i)*deltaTheta;
         if theta(i) <2*pi
             x = SunX + a(i)*cos(theta(i)) +e(i)*a(i);
             y = SunY + b(i)*sin(theta(i));
             plot(x,y,'o');
         end       
-    end  
+    end
+    if ifSaveGif
+    F=getframe(gcf);
+    I=frame2im(F);
+    [I,map]=rgb2ind(I,256);
+    if pic_num == 1
+        imwrite(I,map,'plants.gif','gif', 'Loopcount',inf,'DelayTime',0.2);
+    else
+        imwrite(I,map,'plants.gif','gif','WriteMode','append','DelayTime',0.2);
+    end
+    pic_num = pic_num + 1;
+    else
     pause(sleep);
+    end
 end
